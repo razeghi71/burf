cc250543faa4ea76c80adbf296247a26d80ead66
Mohammad Razeghi <razeghi71@gmail.com>
2023-08-15 20:25:00 +0200

    update the long desc format

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="burf",
-    version="1.0.2",
+    version="1.0.3",
     packages=["burf"],
     install_requires=["textual==0.28.1", "google-cloud-storage==2.10.0"],
     entry_points={"console_scripts": ["burf=burf.burf:main"]},
@@ -10,6 +10,7 @@ setup(
     author_email="razeghi71@gmail.com",
     description="A tool to surf buckets on gcs",
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/razeghi71/burf",
     classifiers=[
         "Development Status :: 3 - Alpha",
