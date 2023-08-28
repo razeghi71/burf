826dd4bfab5d4f6725a3c46fad73cf021386a6a9
Mohammad Razeghi <razeghi71@gmail.com>
2023-08-28 13:09:26 +0200

    change the dowloader screen to be able to close mid-download

--- burf/downloader_screen.py
+++ burf/downloader_screen.py
@@ -1,12 +1,12 @@
 import os
 import threading
+from enum import Enum
 from typing import Iterator
 
-from textual import events
 from textual.app import ComposeResult
-from textual.containers import Center, Middle, Horizontal, Container
+from textual.containers import Center, Container, Horizontal, Middle
 from textual.screen import Screen
-from textual.widgets import Footer, Header, Label, ProgressBar, Button
+from textual.widgets import Button, Footer, Header, Label, ProgressBar
 
 from burf.storage.ds import BucketWithPrefix
 from burf.storage.paths import Blob
@@ -17,10 +17,19 @@ class Downloader:
     def __init__(self, uri: BucketWithPrefix, storage: Storage) -> None:
         self._uri = uri
         self._storage = storage
+        self._stopped = False
 
     @property
     def uri(self) -> BucketWithPrefix:
         return self._uri
+
+    @property
+    def stopped(self):
+        return self._stopped
+
+    @stopped.setter
+    def stopped(self, new):
+        self._stopped = new
 
     def number_of_blobs(self) -> int:
         return len(
@@ -28,22 +37,29 @@ class Downloader:
         )
 
     def download(self, destination_folder: str) -> Iterator[Blob]:
-        for blob in self._storage.list_all_blobs(
-            self._uri.bucket_name, self._uri.prefix
-        ):
-            destination_path = os.path.join(
-                destination_folder, blob.name[len(self._uri.prefix) :]
-            )
-            # TODO: uncomment this and also put everything in the original bucket name
-            # os.makedirs(os.path.dirname(destination_path), exist_ok=True)
-            self._storage.download_to_filename(blob, destination_path)
-            yield blob
+        blobs = self._storage.list_all_blobs(self._uri.bucket_name, self._uri.prefix)
+        for blob in blobs:
+            if not self.stopped:
+                destination_path = os.path.join(
+                    destination_folder, blob.name[len(self._uri.prefix) :]
+                )
+                # TODO: uncomment this and also put everything in the original bucket name
+                # os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+                self._storage.download_to_filename(blob, destination_path)
+                yield blob
+            else:
+                break
+
+
+class State(Enum):
+    STOPPED = 0
+    STARTED = 1
 
 
 class DownloaderScreen(Screen[None]):
     BINDINGS = [
-        ("ctrl+d", "app.pop_screen", "cancel"),
-        ("escape", "app.pop_screen", "close"),
+        ("escape", "close", "close"),
+        ("ctrl+d", "close", "close"),
     ]
 
     CSS = """
@@ -72,9 +88,10 @@ class DownloaderScreen(Screen[None]):
     ) -> None:
         super().__init__(name, id, classes)
         self._downloader = Downloader(download_uri, storage)
+        self.state = State.STOPPED
 
-    def update_progress(self) -> None:
-        for blob in self._downloader.download(os.path.expanduser(".")):
+    def start_download(self) -> None:
+        for blob in self._downloader.download("."):
             self.progress.advance(1)
             self.label.update(f"Downloaded {blob.name}")
         self.label.update("Download Finished")
@@ -88,9 +105,10 @@ class DownloaderScreen(Screen[None]):
 
         with Container(id="question"):
             with Center():
-                yield Label(
+                self.question_label = Label(
                     f"Proceed Downloading {self._downloader.uri} => {download_to}"
                 )
+                yield self.question_label
 
             with Horizontal(id="horizontal"):
                 with Center():
@@ -104,10 +122,27 @@ class DownloaderScreen(Screen[None]):
                 yield self.progress
         yield Footer()
 
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        if event.button.id == "yes":
-            self.query_one("#question").styles.display = "none"
-            self.query_one("#downloader").styles.display = "block"
-            threading.Thread(target=self.update_progress).start()
+    def action_close(self):
+        if self.state == State.STARTED:
+            self.query_one("#question").styles.display = "block"
+            self.question_label.update("Do you want to stop the download?")
         else:
             self.dismiss()
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if self.state == State.STOPPED:
+            if event.button.id == "yes":
+                self.query_one("#question").styles.display = "none"
+                self.query_one("#downloader").styles.display = "block"
+                self.state = State.STARTED
+                self._downloader.stopped = False
+                threading.Thread(target=self.start_download).start()
+            else:
+                self.dismiss()
+        elif self.state == State.STARTED:
+            if event.button.id == "yes":
+                self._downloader.stopped = True
+                self.dismiss()
+            else:
+                self.query_one("#question").styles.display = "none"
+                self.query_one("#downloader").styles.display = "block"
